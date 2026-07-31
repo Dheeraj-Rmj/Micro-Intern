6e2114cf9f61d6d3b4475f637d12ed12dd1b119e
vyshnavi nakka <vyshnavi2409@gmail.com>
2026-07-31 13:21:39 +0530

    chore: remove ReactQueryDevtools floating icon

--- apps/web/src/providers/index.tsx
+++ apps/web/src/providers/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'next-themes';
 import { useState } from 'react';
 import { Toaster } from 'sonner';
@@ -70,10 +69,6 @@ export function Providers({ children }: { children: React.ReactNode }) {
           />
           </AppProvider>
         </AuthProvider>
-        <ReactQueryDevtools
-          initialIsOpen={false}
-          buttonPosition="bottom-left"
-        />
       </QueryClientProvider>
     </ThemeProvider>
   );
